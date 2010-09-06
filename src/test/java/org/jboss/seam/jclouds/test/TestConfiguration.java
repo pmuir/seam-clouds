0fb1086bd0c951eeb74b0580eae00caea48e5dd2
Pete Muir <pmuir@bleepbleep.org.uk>
2010-09-06 13:23:51 +0100

    oops

--- src/test/java/org/jboss/seam/jclouds/test/TestConfiguration.java
+++
@@ -1,5 +0,0 @@
-Unstaged changes after reset:
-M	pom.xml
-M	src/main/java/org/jboss/seam/jclouds/BlobStoreContextFactoryManager.java
-M	src/main/java/org/jboss/seam/jclouds/BlobStoreManager.java
-M	src/test/java/org/jboss/seam/jclouds/test/TestConfiguration.java
